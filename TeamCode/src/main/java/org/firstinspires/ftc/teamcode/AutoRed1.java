b18556d580784d9a8ba22fc06fd1037e2ca87f47
abush2024 <73666145+abush2024@users.noreply.github.com>
2021-10-04 17:10:34 -0700

    Update AutoRed1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed1.java
@@ -1,4 +1,46 @@
+
+
 package org.firstinspires.ftc.teamcode;
 
-public class AutoRed1 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+
+@Autonomous
+@Disabled
+public class AutoRed1 extends LinearOpMode {
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    @Override
+    public void runOpMode() {
+
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
 }
