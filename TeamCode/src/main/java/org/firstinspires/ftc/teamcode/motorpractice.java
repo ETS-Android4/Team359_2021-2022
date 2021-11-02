d67cee1ac93d0c5bb9ccdde61aeeda88484e58b8
abush2024 <73666145+abush2024@users.noreply.github.com>
2021-11-02 16:12:40 -0700

    Update motorpractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorpractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorpractice.java
@@ -1,82 +1,74 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.hardware.DcMotor;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@Autonomous(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+@TeleOp(name = "jonathanTest")
 public class motorpractice extends LinearOpMode {
 
-    // Declare OpMode members.
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
-    private DcMotor side;
-
-    @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        side = hardwareMap.get(DcMotor.class, "side");
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    DcMotor motorMiddle;
+    DcMotor Intake;
 
 
-        // Wait for the game to start (driver presses PLAY)
+    float leftMotorPower = 0f;
+    float rightMotorPower = 0f;
+    float middleMotorPower = 0f;
+
+    public void runOpMode() throws InterruptedException {
+
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorMiddle = hardwareMap.dcMotor.get("motorMiddle");
+        Intake = hardwareMap.dcMotor.get("motorMiddle");
+
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
         while (opModeIsActive()) {
+            telemetry.addData("opModeIsActive", opModeIsActive());
+            telemetry.update();
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            //double leftPower;
-            //double rightPower;
+            leftMotorPower = gamepad1.left_stick_y;
+            rightMotorPower = gamepad1.right_stick_y;
+            middleMotorPower = gamepad1.right_stick_x;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+            if(gamepad1.x)
+            {
+                Intake.setPower(-1);
+            }
+            else
+            {
+                Intake.setPower(gamepad1.right_trigger);
+            }
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            //double drive = -gamepad1.left_stick_y;
-            //double turn  =  gamepad1.right_stick_x;
-            //leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            //rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            if (Math.abs(gamepad1.right_stick_x) > 0 && Math.abs(gamepad1.left_stick_y) > 0) {
+                if(gamepad1.right_stick_x > 0){
+                    leftMotorPower = 0;
+                    rightMotorPower = gamepad1.right_stick_x;
+                }
+                else if (gamepad1.right_stick_x < 0){
+                    rightMotorPower = 0;
+                    leftMotorPower = gamepad1.right_stick_x;
+                }
+            }
+            else if (Math.abs(gamepad1.right_stick_x) > 0){
+                if(gamepad1.right_stick_x > 0){
+                    leftMotorPower = -gamepad1.right_stick_x;
+                    rightMotorPower = gamepad1.right_stick_x;
+                }
+                else if (gamepad1.right_stick_x < 0){
+                    rightMotorPower = gamepad1.right_stick_x;
+                    leftMotorPower = -gamepad1.right_stick_x;
+                }
+            }
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            leftDrive.setPower(0.8);
-            rightDrive.setPower(0.8);
-            sleep(2000);
-            side.setPower(0.8);
-            sleep(2000);
-            // Show the elapsed game time and wheel power.
-            //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            //telemetry.update();
+            motorLeft.setPower(leftMotorPower);
+            motorRight.setPower(rightMotorPower);
+            motorMiddle.setPower(middleMotorPower);
         }
     }
 }
