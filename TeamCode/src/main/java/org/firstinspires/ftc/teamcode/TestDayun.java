7122ac07cd137bc6914cb3e9efe93810fa5aacaa
DayunSuh <73674333+DayunSuh@users.noreply.github.com>
2021-11-04 16:23:40 -0700

    TestDayun Upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDayun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDayun.java
@@ -1,26 +1,24 @@
-
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
 
 @Autonomous
-public class AutoRed1 extends LinearOpMode {
+
+public class TestDayun extends LinearOpMode {
 
     DcMotor leftDrive;
     DcMotor rightDrive;
-    DcMotor linearSlide;
-    DcMotor middle;
 
     @Override
     public void runOpMode() {
 
         leftDrive = hardwareMap.dcMotor.get("leftDrive");
         rightDrive = hardwareMap.dcMotor.get("rightDrive");
-        linearSlide = hardwareMap.dcMotor.get("linearSlide");
-        middle = hardwareMap.dcMotor.get("middle");
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -37,10 +35,6 @@ public class AutoRed1 extends LinearOpMode {
             // Send calculated power to wheels
             leftDrive.setPower(1);
             rightDrive.setPower(1);
-            linearSlide.setPower(1);
-            middle.setPower(1);
-
-            sleep(3000);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", 1, 1);
@@ -48,3 +42,5 @@ public class AutoRed1 extends LinearOpMode {
         }
     }
 }
+
+
