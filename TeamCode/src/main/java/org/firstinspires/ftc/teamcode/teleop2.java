e429e2ad05268933e90b9465c57e36e47bbde5be
abush2024 <73666145+abush2024@users.noreply.github.com>
2021-11-04 16:22:49 -0700

    Update teleop2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2.java
@@ -6,8 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
 @TeleOp
 public class teleop2 extends LinearOpMode {
+
 
     DcMotor leftDrive;
     DcMotor rightDrive;
@@ -15,30 +17,123 @@ public class teleop2 extends LinearOpMode {
     DcMotor linSlide;
     Servo Carousel;
 
+    boolean reverse = false;
     @Override
     public void runOpMode() {
+            double speed = gamepad1.right_trigger - gamepad1.left_trigger;
+            double direction = -gamepad1.left_stick_x;
 
-        leftDrive = hardwareMap.dcMotor.get("leftDrive");
-        rightDrive = hardwareMap.dcMotor.get("rightDrive");
-        Intake = hardwareMap.dcMotor.get("Intake");
-        linSlide = hardwareMap.dcMotor.get("linSlide");
+            if(gamepad1.x) {
+                reverse = false;
+            } else if(robot.gamepad1.y) {
+                reverse = true;
+            }
 
-        waitForStart();
+            DriveDir driveDir = getDir(direction);
 
-        while (opModeIsActive()) {
+            if(Math.abs(speed) < 5 && driveDir != DriveDir.STRAIGHT) {
+                drive(-direction, direction);
+            } else {
+                if (reverse) {
+                    drive(driveDir.getLeft(reverse) * speed, driveDir.getRight(reverse) * speed);
+                } else {
+                    drive(driveDir.getRight(reverse) * speed, driveDir.getLeft(reverse) * speed);
+                }
+            }
 
-            double leftPower;
-            double rightPower;
+            robot.telemetry.addData("Drive - Dat - speed", speed);
+            robot.telemetry.addData("Drive - Dat - direction", driveDir.name());
+            robot.telemetry.addData("Drive - Set - frontLeft", frontLeft.getPower());
+            robot.telemetry.addData("Drive - Set - backLeft", backLeft.getPower());
+            robot.telemetry.addData("Drive - Set - frontRight", frontRight.getPower());
+            robot.telemetry.addData("Drive - Set - backRight", backRight.getPower());
+            robot.telemetry.addData("Drive - Enc - Left", frontLeft.getCurrentPosition());
+            robot.telemetry.addData("Drive - Enc - Right", frontRight.getCurrentPosition());
+        }
+
+        private DriveDir getDir(double dir) {
+            if(dir <= -0.7) {
+                return DriveDir.HARD_LEFT;
+            } else if(dir <= -0.4) {
+                return DriveDir.MEDIUM_LEFT;
+            } else if(dir <= -0.1) {
+                return DriveDir.EASY_LEFT;
+            } else if(dir >= 0.7) {
+                return DriveDir.HARD_RIGHT;
+            } else if(dir >= 0.4) {
+                return DriveDir.MEDIUM_RIGHT;
+            } else if(dir >= 0.1) {
+                return DriveDir.EASY_RIGHT;
+            } else {
+                return DriveDir.STRAIGHT;
+            }
+        }
+
+        public enum DriveDir {
+            HARD_LEFT(-1, 1),
+            MEDIUM_LEFT(0, 1),
+            EASY_LEFT(0.5, 1),
+            STRAIGHT(1, 1),
+            EASY_RIGHT(1, 0.5),
+            MEDIUM_RIGHT(1, 0),
+            HARD_RIGHT(1, -1);
+
+            private double left, right;
+
+            DriveDir(double left, double right) {
+                this.left = left;
+                this.right = right;
+            }
+
+            public double getLeft(boolean reverse) {
+                return reverse ? -left : left;
+            }
+
+            public double getRight(boolean reverse) {
+                return reverse ? -right : right;
+            }
+        }
+
+        private void left(double power) {
+            try {
+                frontLeft.setPower(power);
+                backLeft.setPower(power);
+            } catch(Exception ex) {}
+        }
+
+        private void right(double power) {
+            try {
+                frontRight.setPower(power);
+                backRight.setPower(power);
+            } catch(Exception ex) {}
+        }
+
+        public void drive(double left, double right) {
+            left(left);
+            right(right);
+        }
+
+        public void drive(double speed, DriveDir driveDir) {
+            if(reverse) {
+                drive(driveDir.getLeft(reverse) * speed, driveDir.getRight(reverse) * speed);
+            } else {
+                drive(driveDir.getRight(reverse) * speed, driveDir.getLeft(reverse) * speed);
+            }
+        }
+
+        public void stop() {
+            drive(0, 0);
+        }
 
 
+        int base = 0;
+        public void resetEncoder() {
+            base = backRight.getCurrentPosition();
+        }
 
-            // Send calculated power to wheels
-            leftDrive.setPower(1);
-            rightDrive.setPower(1);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", 1, 1);
-            telemetry.update();
+        public int getEncoder() {
+            return backRight.getCurrentPosition() - base;
+        }
         }
     }
 }
